a9b95ce9d3fcc557aaa6c10e8df694139102525e
SuL <jjw011202@naver.com>
2021-07-11 20:38:12 +0900

    처음으로 입장 시, world가 아닌 원하는 곳으로 티피

---
+++ src/main/kotlin/kr/sul/miscellaneousthings2/something/GotoSpawnWhenFirstJoin.kt
@@ -0,0 +1,21 @@
+package kr.sul.miscellaneousthings2.something
+
+import kr.sul.miscellaneousthings2.Main.Companion.plugin
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+
+object GotoSpawnWhenFirstJoin: Listener {
+
+    // TODO 너무 대충함
+    @EventHandler
+    fun onJoin(e: PlayerJoinEvent) {
+        Bukkit.getScheduler().runTask(plugin) {
+            if (e.player.location.world.name == "world") {  // 첫번째 스폰 시 world로 감
+                e.player.teleport(Location(Bukkit.getWorld("SPAWN-2"), 1785.5, 50.0, 568.5))
+            }
+        }
+    }
+}
