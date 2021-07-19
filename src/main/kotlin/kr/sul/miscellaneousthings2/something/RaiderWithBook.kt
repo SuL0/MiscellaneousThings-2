87c63ff05849a697c5e5c4fcaaedff49b49cc67e
SuL <jjw011202@naver.com>
2021-07-19 20:32:39 +0900

    책 아이템으로 레이더(글로우) 완성

--- src/main/kotlin/kr/sul/miscellaneousthings2/something/RaiderWithBook.kt
+++ src/main/kotlin/kr/sul/miscellaneousthings2/something/RaiderWithBook.kt
@@ -1,56 +1,62 @@
 package kr.sul.miscellaneousthings2.something
 
+import kr.sul.servercore.util.ItemBuilder.durabilityIB
 import kr.sul.servercore.util.ItemBuilder.nameIB
+import kr.sul.servercore.util.PlayerDistance.distance
 import net.md_5.bungee.api.chat.BaseComponent
 import org.apache.commons.lang.StringUtils
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
-import org.bukkit.event.player.PlayerDropItemEvent
-import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.event.player.PlayerSwapHandItemsEvent
+import org.bukkit.event.player.*
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.PlayerInventory
+import org.inventivetalent.glow.GlowAPI
 import xyz.upperlevel.spigot.book.BookUtil
+import xyz.upperlevel.spigot.book.BookUtil.PageBuilder
+import java.util.*
 import kotlin.math.round
 
-object RaiderWithBook: Listener {
-    private val BOOK_MATERIAL = Material.COCOA
-    private val bookItem: ItemStack
-        get() = ItemStack(BOOK_MATERIAL).nameIB("§c[ §f레이더 §c]")
 
+object RaiderWithBook: Listener {
+    private val bookItem: ItemStack
+        get() = ItemStack(Material.INK_SACK).durabilityIB(3).nameIB("§c[ §f레이더 §c]")
     private const val NEAR_DISTANCE = 50
     private const val FAR_DISTANCE = 100
     private const val LINES_PER_PAGE = 13
 
+    private val glow = hashMapOf<Player, ArrayList<UUID>>()  // Key가 보고 있는 글로우 상대를 Value에 저장
 
     private fun openRaiderBook(p: Player) {
-        // TODO p.world.entities 수정
-        val detectedPlayers = p.world.entities.filter { it.location.distance(p.location) <= FAR_DISTANCE && it != p }
+        val detectedPlayers = p.world.players.filter { it.distance(p) <= FAR_DISTANCE && it != p }
             .sortedBy { it.location.distance(p.location) }
+
+        if (glow.containsKey(p)) {
+            removeUselessGlow(p, detectedPlayers)
+        }
 
         val book = BookUtil.writtenBook()
         val tempPageStorage = arrayListOf<Array<BaseComponent>>()
-        var pageBuilder: BookUtil.PageBuilder? = null
+        var pageBuilder: PageBuilder? = null
         for ((cnt, detectedPlayer) in detectedPlayers.withIndex()) {
             if (cnt % LINES_PER_PAGE == 0) {
                 if (cnt != 0) {  // cnt == 0은 예외
                     tempPageStorage.add(pageBuilder!!.build())
                 }
-                pageBuilder = BookUtil.PageBuilder()  //
+                pageBuilder = PageBuilder()  //
                 pageBuilder.add("§4§l플레이어  거리  소속길드").newLine()
             }
             val name = StringUtils.rightPad(detectedPlayer.type.name, 10, ' ')
-            val distance = round(detectedPlayer.location.distance(p.location)*10)/10
+            val distance = round(detectedPlayer.distance(p)*10)/10
             if (distance > NEAR_DISTANCE && distance <= FAR_DISTANCE) {  // 50~100칸   (&k 로 인해서 거리가 제대로 보이지 않음)
                 pageBuilder!!.add("$name §k[${distance}]").newLine()
             } else {  // 50칸 이내
                 pageBuilder!!.add("$name ${distance}m").newLine()
-//                GlowAPI.setGlowing(detectedPlayer, GlowAPI.Color.RED, p)   // TODO <= 50인 엔티티 3초간 글로우
+                glowDetectedPlayer(p, detectedPlayer as Player)
             }
         }
         val bookItem = book.author("").title("").pages(tempPageStorage).build()
@@ -58,13 +64,35 @@ object RaiderWithBook: Listener {
     }
 
 
+    // glow[p] (List<UUID>) 에서 playersToStay 에 포함되지 않는 값은 모두 글로우 해제하고 값 제거
+    private fun removeUselessGlow(p: Player, playersToStay: List<Player>) {
+        val playersToStayUUID = playersToStay.map { it.uniqueId }
+        val playersToRemoveUUID = glow[p]!!.filter { !playersToStayUUID.contains(it) }
+        for (pToRemoveUUID in playersToRemoveUUID) {
+            val pToRemove = Bukkit.getPlayer(pToRemoveUUID)
+                ?: continue  // pToRemove가 Offline 유저일 때
+            GlowAPI.setGlowing(pToRemove, false, p)
+        }
+        glow[p]!!.removeAll(playersToRemoveUUID)
+    }
+
+    private fun glowDetectedPlayer(p: Player, detectedPlayer: Player) {
+        if (!glow.containsKey(p)) {
+            glow[p] = arrayListOf()
+        }
+        if (!glow[p]!!.contains(detectedPlayer.uniqueId)) {  // 만약 이미 Glow가 된 상태라면 다시 Glow를 시키지는 않음
+            glow[p]!!.add(detectedPlayer.uniqueId)
+            GlowAPI.setGlowing(detectedPlayer, GlowAPI.Color.DARK_PURPLE, p)
+        }
+    }
+
 
 
 
     // BOOK 핫바 9번 슬롯에 고정
     @EventHandler
     fun onRightClickBook(e: PlayerInteractEvent) {
-        if (e.player.inventory.itemInMainHand.type == BOOK_MATERIAL) {
+        if (e.player.inventory.itemInMainHand.isSimilar(bookItem)) {
             e.isCancelled = true
             openRaiderBook(e.player)
         }
@@ -73,7 +101,7 @@ object RaiderWithBook: Listener {
     fun onJoin(e: PlayerJoinEvent) {
         val p = e.player
         val pInv = e.player.inventory
-        if (pInv.getItem(8) != null && pInv.getItem(8).type != BOOK_MATERIAL) {  // 만약 8번 슬롯에 무언가 아이템이 있는 경우, 해당 아이템을 옮겨줌
+        if (pInv.getItem(8) != null && pInv.getItem(8).isSimilar(bookItem)) {  // 만약 8번 슬롯에 무언가 아이템이 있는 경우, 해당 아이템을 옮겨줌
             val itemToMove = pInv.getItem(8)
             if (pInv.firstEmpty() != -1) {  // 인벤이 꽉차지 않았을 때
                 pInv.addItem(itemToMove)
@@ -93,15 +121,20 @@ object RaiderWithBook: Listener {
     @EventHandler(priority = EventPriority.LOW)
     fun onDrop(e: PlayerDropItemEvent) {
         if (e.isCancelled) return
-        if (e.itemDrop.itemStack.type == BOOK_MATERIAL) {
+        if (e.itemDrop.itemStack.isSimilar(bookItem)) {
             e.isCancelled = true
         }
     }
     @EventHandler(priority = EventPriority.LOW)
     fun onSwap(e: PlayerSwapHandItemsEvent) {
         if (e.isCancelled) return
-        if (e.offHandItem.type == BOOK_MATERIAL) {
+        if (e.offHandItem.isSimilar(bookItem)) {
             e.isCancelled = true
         }
+    }
+
+    @EventHandler
+    fun onQuit(e: PlayerQuitEvent) {
+        glow.remove(e.player)
     }
 }
