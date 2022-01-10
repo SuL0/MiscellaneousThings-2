632a5a25f50b1b3f3a5136efcdeeb2c33f5ced39
SuL <sul0332u@gmail.com>
2022-01-10 14:36:45 +0900

    스폰 시간 12000~13500으로 자연스럽게 고정, 비치타운 시간 고정

--- src/main/kotlin/kr/sul/miscellaneousthings2/something/world/FixTimeInSomeWorlds.kt
+++ src/main/kotlin/kr/sul/miscellaneousthings2/something/world/FixTimeInSomeWorlds.kt
@@ -1,26 +1,70 @@
 package kr.sul.miscellaneousthings2.something.world
 
+import com.github.shynixn.mccoroutine.launch
+import kotlinx.coroutines.delay
 import kr.sul.miscellaneousthings2.Main.Companion.plugin
+import kr.sul.servercore.util.ClassifyWorlds
 import org.bukkit.Bukkit
 
 object FixTimeInSomeWorlds {
-    private const val TERM = 10*20.toLong()
+
+    private const val TERM = 1*20.toLong()
     init {
         Bukkit.getScheduler().runTask(plugin) {
-            val worlds = Bukkit.getWorlds().filter { it.name.startsWith("Spawn", true) || it.name.startsWith("Normal", true) }
-            val hardTestWorld = Bukkit.getWorld("HardTest")
-
             Bukkit.getScheduler().runTaskTimer(plugin, {
                 // 낮, 비X 고정
-                worlds.forEach {
-                    it.time = 6000  // 낮
+                ClassifyWorlds.beachTownWorlds.forEach {
+                    it.time = 12500
                     it.setStorm(false)  // 비 X
                 }
-
-                // 하드 테스트, 23시 고정, 비X
-                hardTestWorld?.time = 23000
-                hardTestWorld?.setStorm(false)  // 비 X
             }, 0, TERM)
         }
+    }
+
+    init {
+        SpawnTimeCycle.dummy() // 초기화 용도
+    }
+
+    // 10분에 한 사이클 (12000 -> 13500 or 13500 -> 12000)
+    object SpawnTimeCycle {
+        private const val MIN = 12000
+        private const val MAX = 13500
+        private const val SCHEDULER_TERM = 20L
+        private const val ONE_CYCLE_TERM = 10*60*20L
+//        private const val TERM = 1*10*20L
+        private const val smoothen = (MAX-MIN)*(SCHEDULER_TERM/ONE_CYCLE_TERM.toDouble())
+//        private const val smoothen = 500*((1*20)/TERM.toDouble())
+
+
+        private var currentTime = MIN.toDouble()
+        private var direction = 1
+        init {
+            plugin.launch {
+                delay(50L)
+                val spawnWorlds = Bukkit.getWorlds().filter { it.name.startsWith("Spawn", true) }
+
+                Bukkit.getScheduler().runTaskTimer(plugin, {
+                    currentTime += (smoothen* direction)
+                    spawnWorlds.forEach {
+                        it.time = currentTime.toLong()
+                        it.setStorm(false)
+                    }
+                    if (currentTime <= MIN || MAX <= currentTime) {
+                        reverseDirection()
+                    }
+                }, 0L, SCHEDULER_TERM)
+            }
+        }
+
+        private fun reverseDirection() {
+            direction = if (direction == 1) {
+                currentTime = MAX.toDouble() // 이건 굳이 할 필요는 없는데, 명령어 또는 다른 요인에 의해 강제로 시간이 변경됐을 때 시간을 다시 정상으로 되돌려 놓기 위한 장치
+                -1
+            } else {
+                currentTime = MIN.toDouble()
+                1
+            }
+        }
+        fun dummy() { }
     }
 }
