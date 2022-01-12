363659452393d5648c481032e92e9d4be730b044
SuL <sul0332u@gmail.com>
2022-01-12 14:49:42 +0900

    /elag 명령어 일반 플레이어가 실행 못 하게

---
+++ src/main/kotlin/kr/sul/miscellaneousthings2/something/block/BlockSomeCommand.kt
@@ -0,0 +1,16 @@
+package kr.sul.miscellaneousthings2.something.block
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerCommandPreprocessEvent
+
+object BlockSomeCommand: Listener {
+    @EventHandler(priority = EventPriority.HIGH)
+    fun onCommand(e: PlayerCommandPreprocessEvent) {
+        if (e.isCancelled || e.player.isOp) return
+        if (e.message.startsWith("/elag")) {
+            e.isCancelled = true
+        }
+    }
+}
