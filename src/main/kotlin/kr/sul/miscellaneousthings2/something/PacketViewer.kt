4950f5cdbf2c39313efa89236c67ca5a8293a512
SuL <sul0332u@gmail.com>
2022-05-02 14:06:45 +0900

    ArmorWeight 비활성화, 기타

--- src/main/kotlin/kr/sul/miscellaneousthings2/something/PacketViewer.kt
+++ src/main/kotlin/kr/sul/miscellaneousthings2/something/PacketViewer.kt
@@ -6,18 +6,39 @@ import com.comphenix.protocol.events.ListenerPriority
 import com.comphenix.protocol.events.PacketAdapter
 import com.comphenix.protocol.events.PacketEvent
 import kr.sul.miscellaneousthings2.Main.Companion.plugin
+import org.apache.commons.lang.StringUtils
 import org.bukkit.Bukkit
+import org.bukkit.inventory.ItemStack
 
 object PacketViewer {
     private val pm = ProtocolLibrary.getProtocolManager()
 
     fun register() {
+        return
+//        pm.addPacketListener(object : PacketAdapter(plugin, ListenerPriority.LOWEST, PacketType.values()) {
+//            override fun onPacketReceiving(e: PacketEvent) {
+//                Bukkit.broadcastMessage("§7 - §f${e.packetType.name()}")
+//            }
+//        })
         pm.addPacketListener(object : PacketAdapter(plugin, ListenerPriority.LOWEST, PacketType.values()) {
-            override fun onPacketReceiving(e: PacketEvent) {
-                if (e.packetType != PacketType.Play.Client.POSITION && e.packetType != PacketType.Play.Client.POSITION_LOOK && e.packetType != PacketType.Play.Client.LOOK) {  // 도배하는 패킷
-                    Bukkit.broadcastMessage("§7 - §f${e.packetType.name()}")
+            override fun onPacketSending(e: PacketEvent) {
+                if (e.packetType == PacketType.Play.Server.CHAT
+                    || e.packetType == PacketType.Play.Server.TRANSACTION
+                    || e.packetType == PacketType.Play.Server.PLAYER_INFO
+                    || e.packetType == PacketType.Play.Server.UPDATE_TIME
+                    || e.packetType == PacketType.Play.Server.MAP_CHUNK
+                    || e.packetType == PacketType.Play.Server.UNLOAD_CHUNK
+                ) {
+                    return
                 }
             }
+
+//            override fun onPacketReceiving(e: PacketEvent) {
+//                if (e.packetType == PacketType.Play.Client.POSITION) {
+//                    val str = StringUtils.join(e.packet.doubles.values.toTypedArray(), ", ")
+//                    Bukkit.broadcastMessage("$str")
+//                }
+//            }
         })
     }
 }
--- src/main/kotlin/kr/sul/miscellaneousthings2/something/SendResourcePack.kt
+++ src/main/kotlin/kr/sul/miscellaneousthings2/something/SendResourcePack.kt
@@ -21,12 +21,12 @@ object SendResourcePack: Listener {
             p.sendMessage("§6§lRP: §f서버 리소스팩을 다운로드 받는 중입니다.")
             p.sendMessage(" §7└ 렉이 발생할 수 있습니다.")
             p.setResourcePack(URL, HASH)  // hash(sha1) 사이트: http://onlinemd5.com/
-            // TODO: 일단 스폰에 이동시키기 or 투명 무적 걸기
+            // TODO: 일단 스폰에 이동시키기 or 투명 무적 블라인드 걸기. 필요하다면 패킷으로 눈 앞이나 플레이어 머리가 끼이게끔 블럭을 두는 것도 괜찮을 듯?
         }, 5L)
     }
 
     @EventHandler
     fun onResourcePackReceived(e: PlayerResourcePackStatusEvent) {
-        // TODO: 다시 이전 위치로 돌려보내기 or 투명 무적 풀기
+        // TODO: 다시 이전 위치로 돌려보내기 or 투명 무적 블라인드 풀기
     }
 }
