c196950cbd8d981337e096a6de39de7eb77e0ec6
SuL <sul0332u@gmail.com>
2022-05-20 00:39:55 +0900

    음식, 근접무기 Defined(enum)

--- src/main/kotlin/kr/sul/miscellaneousthings2/customitem/food/FoodDefined.kt
+++ src/main/kotlin/kr/sul/miscellaneousthings2/customitem/food/FoodDefined.kt
@@ -12,7 +12,7 @@ enum class FoodDefined(
 ): NormalItem {
 
     COOKIE("bea9fb28-e181-4150-b66e-dbbfcad3bf3a",
-        ItemStackBasicInfo(Material.COOKIE, "§6쿠키", null),
+        ItemStackBasicInfo(Material.COOKIE, "§6초코칩 쿠키", null),
         2
     ),
     CAKE("4cc3fe66-2e37-43e5-ac4a-cbc10af6183a",
@@ -22,5 +22,49 @@ enum class FoodDefined(
     WATER_MELON("d515334b-2a64-4335-9205-75fe02e5a133",
         ItemStackBasicInfo(Material.MELON, "§6수박", null),
         2
+    ),
+    CHOCOLATE_PIE("b5b18aae-125f-48d8-9607-dd1bd02f658a",
+        ItemStackBasicInfo(Material.CARROT, "§6초코파이", null),
+        3
+    ),
+    RAMYEON("9a35fc31-ff3d-4333-96fc-2d4b09df8b6e",
+        ItemStackBasicInfo(Material.APPLE, "§6생 라면", null),
+        4
+    ),
+    ROTTEN_FLESH("1b35e911-cb66-4ec5-8179-607bf9d80d9e",
+        ItemStackBasicInfo(Material.ROTTEN_FLESH, "§6썩은 고기", null),
+        4
+    ),
+    COOKED_FISH("7dba382d-fe01-4332-8b72-bfcc1456fe07",
+        ItemStackBasicInfo(Material.COOKED_FISH, "§6익힌 생선", null),
+        5
+    ),
+    COOKED_RABBIT("b26ffc4d-7320-4342-bf35-b2f9f871b996",
+        ItemStackBasicInfo(Material.COOKED_RABBIT, "§6토끼 고기", null),
+        5
+    ),
+    POTATO_CHIP("043d3770-31ab-4bfa-bede-5437129acb04",
+        ItemStackBasicInfo(Material.BAKED_POTATO, "§6감자 칩", null),
+        5
+    ),
+    CHOCOLATE_BAR("b34f13de-c1c7-4b5f-b45e-11df819628c4",
+        ItemStackBasicInfo(Material.BREAD, "§6초콜릿 바", null),
+        5
+    ),
+    COOKED_CHICKEN("fd4b95f5-4d8d-45eb-b30b-cafde0059d5d",
+        ItemStackBasicInfo(Material.COOKED_CHICKEN, "§6닭고기", null),
+        5
+    ),
+    COOKED_MUTTON("ce30cdc6-03f8-40de-b497-da4a0d737fec",
+        ItemStackBasicInfo(Material.COOKED_MUTTON, "§6양고기", null),
+        5
+    ),
+    COOKED_PORK("5d01d80e-b92c-4fb5-8623-22ae56a1a217",
+        ItemStackBasicInfo(Material.GRILLED_PORK, "§6돼지고기", null),
+        5
+    ),
+    BOX_OF_CHOCOLATE_PIE("f2503552-dcf9-402a-a711-e1099d1d8683",
+        ItemStackBasicInfo(Material.COOKED_BEEF, "§6초코파이 상자", null),
+        5
     );
 }
--- src/main/kotlin/kr/sul/miscellaneousthings2/customitem/melee/MeleeWeaponDefined.kt
+++ src/main/kotlin/kr/sul/miscellaneousthings2/customitem/melee/MeleeWeaponDefined.kt
@@ -10,23 +10,80 @@ enum class MeleeWeaponDefined(
     override val enumUuid: String,
     val attackDamage: Double,
     val criticalDamage: Double,
-    val attackSpeed: Double,
+    val attackSpeed: Double,  // 단위) 초
     override val maxDurability: Int,
     override val basicInfo: ItemStackBasicInfo,
     val loreFunc: (Int, Int) -> List<String>
 ): DurabilityItem, HasUniqueID {
 
     DURABLE_BRANCH("3ef28fb1-47e9-417a-86f5-492dc70ba3ea",
-        1.0, 1.0, 0.2, 10,
-        ItemStackBasicInfo(Material.WOOD_SWORD,"§f튼튼한 나뭇가지", null),
+        4.0, 6.0, 0.5, 30,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f튼튼한 나뭇가지", null),
         { currentDurability: Int, maxDurability: Int ->
             listOf("§f튼튼한 나뭇가지이다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
         }
+    ),
+    WOODEN_BAT("76d0ea72-1149-4dad-a766-e1ee4f8572cf",
+        4.5, 6.75, 0.5, 50,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f나무 방망이", null),
+        { currentDurability: Int, maxDurability: Int ->
+            listOf("§7설명란은 공란입니다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
+        }
+    ),
+    WOODEN_SWORD("5243adef-76e8-41db-ac04-b1bdc0a40c91",
+        5.0, 7.5, 0.475, 50,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f목검", null),
+        { currentDurability: Int, maxDurability: Int ->
+            listOf("§7설명란은 공란입니다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
+        }
+    ),
+    FRYING_PAN("1c8cec46-f162-4e7d-a0f4-4e0e7aa80ed3",
+        5.5, 8.25, 0.45, 80,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f후라이팬", null),
+        { currentDurability: Int, maxDurability: Int ->
+            listOf("§7설명란은 공란입니다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
+        }
+    ),
+    IRON_PIPE("e5cb9c8e-7d70-4c1c-b86f-ed17a5766dbd",
+        6.0, 9.0, 0.45, 80,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f쇠파이프", null),
+        { currentDurability: Int, maxDurability: Int ->
+            listOf("§7설명란은 공란입니다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
+        }
+    ),
+    HUNTING_KNIFE("a465c18b-7b91-4edc-bdd0-e4df7c5f58da",
+        6.5, 9.75, 0.5, 200,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f헌팅나이프", null),
+        { currentDurability: Int, maxDurability: Int ->
+            listOf("§7설명란은 공란입니다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
+        }
+    ),
+    LONG_SWORD("1376fb00-31a2-4787-9040-05f5a47873cd",
+        7.0, 10.5, 0.475, 1000,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f장검", null),
+        { currentDurability: Int, maxDurability: Int ->
+            listOf("§7설명란은 공란입니다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
+        }
+    ),
+    BEAM_SABER("dc1f5811-c011-4af9-ac2c-b247ebfb2575",
+        7.5, 11.25, 0.45, 1000,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f빔사벨", null),
+        { currentDurability: Int, maxDurability: Int ->
+            listOf("§7설명란은 공란입니다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
+        }
+    ),
+    LIGHT_SABER("0c43dd39-29f8-45bd-949a-2396c02344f2",
+        8.0, 12.0, 0.425, 1000,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f라이트세이버", null),
+        { currentDurability: Int, maxDurability: Int ->
+            listOf("§7설명란은 공란입니다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
+        }
+    ),
+    BEAM_X("7b892a4a-2739-4918-a694-2392b2ae3cad",
+        8.5, 12.75, 99.99, 1000,
+        ItemStackBasicInfo(Material.GOLD_SWORD,"§f빔엑스", null),
+        { currentDurability: Int, maxDurability: Int ->
+            listOf("§7설명란은 공란입니다.", "", "§7내구도 : §f$currentDurability/$maxDurability")
+        }
     );
-
-//    DURABLE_BRANCH("3ef28fb1-47e9-417a-86f5-492dc70ba3ea",
-//    ItemStackBasicInfo2(Material.WOOD_SWORD, "§f튼튼한 나뭇가지", listOf("§f튼튼한 나뭇가지이다.")),
-//    1, 1, 0.2, 10
-//    );
-
 }
